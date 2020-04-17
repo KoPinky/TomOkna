628e2c8b89cf38fd460b81b21342156eedd5e3bd
Fray <Fray@DAWNTOWN>
2020-04-17 10:01:38 +0700

    nothing

--- TomOkna/Program.cs
+++ TomOkna/Program.cs
@@ -16,7 +16,7 @@ namespace TomOkna
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
